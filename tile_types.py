c51dfef00ce1dbc35c3764697cb3684c196a72a4
gossyozzy <gossyozzy@gmail.com>
2026-06-27 15:49:23 -0600

    Added visibility functionality

--- tile_types.py
+++ tile_types.py
@@ -5,18 +5,19 @@ import numpy as np # type: ignore
 # Tile graphics structured type compatible with Console.tiles_rgb.
 graphic_dt = np.dtype(
     [
-        ("ch", np.int32), # Unicode codepoint.
-        ("fg", "3B"), # 3 unsigned bytes for RGB foreground color.
-        ("bg", "3B"), # 3 unsigned bytes for RGB background color.
+        ("ch", np.int32),  # Unicode codepoint.
+        ("fg", "3B"),  # 3 unsigned bytes for RGB foreground color.
+        ("bg", "3B"),  # 3 unsigned bytes for RGB background color.
     ]
 )
 
 # Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool), # True if this tile can be walked over.
-        ("transparent", np.bool), # True if this tile doesn't block FOV.
-        ("dark", graphic_dt), # Graphics for when this tile is not in FOV.
+        ("walkable", np.bool),  # True if this tile can be walked over.
+        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),  # Graphics for when the tile is in FOV.
     ]
 )
 
@@ -26,15 +27,27 @@ def new_tile(
         walklable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    """ Helper function for defining individual tile types """
-    return np.array((walklable, transparent, dark), dtype=tile_dt)
+    """
+    Helper function for defining individual tile types 
+    """
+    return np.array((walklable, transparent, dark, light), dtype=tile_dt)
+
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 
 floor = new_tile(
-    walklable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walklable=True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 
 wall = new_tile(
-    walklable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walklable=False,
+    transparent=False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
